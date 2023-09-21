d9108efe0eb232fa34174ba7b4aee8abd1ca9223
Laureano Ezequiel Alvarez <137580645+Ezeki-23@users.noreply.github.com>
2023-09-21 00:09:53 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const API_URL = import.meta.env.VITE_API_URL || 'https://ezeki-backend.netlify.app/api'
+export const API_URL = import.meta.env.VITE_API_URL || 'https://backend-ezeki-dev.vercel.app/api'
