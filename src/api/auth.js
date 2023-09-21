74e288f4e785e9f8e57007bf18750d6fe1d167ce
Laureano Ezequiel Alvarez <137580645+Ezeki-23@users.noreply.github.com>
2023-09-20 21:39:56 -0300

    Update auth.js

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from './axios'
 
-const API = 'https://backend-ezeki-dev.vercel.app/api'
+const API = 'http://localhost:3000/api'
 
 export const registerRequest = async (user) => axios.post(`${API}/register`, user)
 
