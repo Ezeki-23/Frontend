638b86efe1275fa15d2882662b81cf7582d5654a
Laureano Ezequiel Alvarez <137580645+Ezeki-23@users.noreply.github.com>
2023-09-21 22:56:01 -0300

    Update MyProject.jsx

--- src/pages/MyProject.jsx
+++ src/pages/MyProject.jsx
@@ -10,8 +10,8 @@ function Productos () {
         <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
           <img className="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/720x400" alt="blog" />
           <div className="p-6">
-            <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">Proyecto 1</h2>
-            <h1 className="title-font text-lg font-medium text-gray-900 mb-3">Nombre proyecto 1</h1>
+            <h2 className="tracking-widest text-xs title-font font-medium text-white mb-1">Proyecto 1</h2>
+            <h1 className="title-font text-lg font-medium text-white mb-3">Nombre proyecto 1</h1>
             <p className="leading-relaxed mb-3">Descripción del proyecto.</p>
             <div className="flex items-center flex-wrap ">
               <a className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Ver proyecto
@@ -39,8 +39,8 @@ function Productos () {
         <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
           <img className="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/721x401" alt="blog" />
           <div className="p-6">
-            <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">Proyecto 2</h2>
-            <h1 className="title-font text-lg font-medium text-gray-900 mb-3">Nombre proyecto 2</h1>
+            <h2 className="tracking-widest text-xs title-font font-medium text-white mb-1">Proyecto 2</h2>
+            <h1 className="title-font text-lg font-medium text-white mb-3">Nombre proyecto 2</h1>
             <p className="leading-relaxed mb-3">Descripción del proyecto.</p>
             <div className="flex items-center flex-wrap">
               <a className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Ver proyecto
@@ -68,8 +68,8 @@ function Productos () {
         <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
           <img className="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/722x402" alt="blog" />
           <div className="p-6">
-            <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">Proyecto 3</h2>
-            <h1 className="title-font text-lg font-medium text-gray-900 mb-3">Nombre proyecto 3</h1>
+            <h2 className="tracking-widest text-xs title-font font-medium text-white mb-1">Proyecto 3</h2>
+            <h1 className="title-font text-lg font-medium text-white mb-3">Nombre proyecto 3</h1>
             <p className="leading-relaxed mb-3">Descripción del proyecto.</p>
             <div className="flex items-center flex-wrap ">
               <a className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Ver proyecto
