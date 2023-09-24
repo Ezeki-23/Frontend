243947280559279e5389976d34e30b324d0d5198
Laureano Ezequiel Alvarez <137580645+Ezeki-23@users.noreply.github.com>
2023-09-24 18:08:06 -0300

    Update auth.js

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from './axios'
 
-const API = 'https://mi-back.vercel.app/api'
+const API = 'https://mi-back-serv.onrender.com/api'
 
 export const registerRequest = async (user) => axios.post(`${API}/register`, user)
 
