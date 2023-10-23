354f16bfa1f6f04da24497fcbc220a8bd709d0a4
Laureano Ezequiel Alvarez <137580645+Ezeki-23@users.noreply.github.com>
2023-10-22 23:56:59 -0300

    Update MyProject.jsx

--- src/pages/MyProject.jsx
+++ src/pages/MyProject.jsx
@@ -8,7 +8,7 @@ function Productos () {
     <div className="flex flex-wrap -m-4">
       <div className="p-4 md:w-1/3">
         <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-          <img className="lg:h-48 md:h-36 w-full object-cover object-center" src="proyecto_1" alt="blog" />
+          <img className="lg:h-48 md:h-36 w-full object-cover object-center" src="proyecto_1.jpg" alt="blog" />
           <div className="p-6">
             <h2 className="tracking-widest text-xs title-font font-medium text-white mb-1">Proyecto 1</h2>
             <h1 className="title-font text-lg font-medium text-white mb-3">Página para una Congregación Cristiana</h1>
