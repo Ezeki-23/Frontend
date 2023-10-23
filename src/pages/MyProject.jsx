0f72825d1d11af1e3e1e27517ad7f34035ab4223
Laureano Ezequiel Alvarez <137580645+Ezeki-23@users.noreply.github.com>
2023-10-23 20:20:48 -0300

    Update MyProject.jsx

--- src/pages/MyProject.jsx
+++ src/pages/MyProject.jsx
@@ -41,7 +41,7 @@ function Productos () {
           <div className="p-6">
             <h2 className="tracking-widest text-xs title-font font-medium text-white mb-1">Proyecto 2</h2>
             <h1 className="title-font text-lg font-medium text-white mb-3">Página Ecommerce</h1>
-            <p className="leading-relaxed mb-3">En este proyecto reforcé mis conocimientos en frontend con React js, Vite y Tailwind CSS. Utilice librerías como axios, react-icons, react-router, etc.. Añadí un pequeño backend donde integre Checkout PRO de Mercado Pago con su SDK para Node. Asi puede el usuario realizar una compra y el vendedor recibir el pago a través del formulario de Mercado Pago. Deje muchisimos botones para seguir añadiendo funcionalidades y expandir el proyecto..</p>
+            <p className="leading-relaxed mb-3">En este proyecto reforcé mis conocimientos en frontend con React js, Vite y Tailwind CSS. Utilice librerías como axios, react-icons, react-router, etc.. Añadí un pequeño backend donde integre Checkout PRO de Mercado Pago con su SDK para Node. Asi el usuario realiza una compra unicamente con tarjeta a través del formulario de Mercado Pago. Deje muchisimos botones para seguir añadiendo funcionalidades y expandir el proyecto..</p>
             <div className="flex items-center flex-wrap">
                <a href="https://ecommer-app.vercel.app" className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Ver proyecto
                 <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
