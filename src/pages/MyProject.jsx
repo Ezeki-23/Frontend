152f28fc94bb40bf62fa37709ec63b75c407c64d
Laureano Ezequiel Alvarez <137580645+Ezeki-23@users.noreply.github.com>
2023-12-27 23:01:30 -0300

    Update MyProject.jsx

--- src/pages/MyProject.jsx
+++ src/pages/MyProject.jsx
@@ -66,13 +66,14 @@ function Productos () {
       </div>
       <div className="p-4 md:w-1/3">
         <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-          <img className="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/722x402" alt="blog" />
+          <img className="lg:h-48 md:h-36 w-full object-cover object-center" src="proyect-3.jpg" alt="proyecto 3" />
           <div className="p-6">
             <h2 className="tracking-widest text-xs title-font font-medium text-white mb-1">Proyecto 3</h2>
-            <h1 className="title-font text-lg font-medium text-white mb-3">Nombre proyecto 3</h1>
-            <p className="leading-relaxed mb-3">Descripción del proyecto.</p>
+            <h1 className="title-font text-lg font-medium text-white mb-3">Ecommerce MERN Full Stack</h1>
+            <p className="leading-relaxed mb-3">Es un Ecommerce más completo e independiente del segundo. Utilicé Vite js con React js para el frontend, y Node js, Express y MongoDB para el backend. El estilador que utilicé es Tailwind CSS. Incorporé React-Redux. Todos los detalles en mi curriculum.  
+</p>
             <div className="flex items-center flex-wrap ">
-              <a className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Ver proyecto
+              <a href="https://commercomplet.vercel.app" className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Ver proyecto
                 <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
                   <path d="M5 12h14"></path>
                   <path d="M12 5l7 7-7 7"></path>
